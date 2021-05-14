d12b1ab40a337c25e63f04193a2aeb50e387262e
Dilain Gunasekara <adsgunasekara18@gmail.com>
2021-05-14 09:29:16 +0530

    search added to products

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,5 +1,5 @@
-import React, { useEffect} from 'react';
-import { Grid, CircularProgress } from "@material-ui/core";
+import React, { useEffect, useState } from 'react';
+import { Grid, CircularProgress, TextField } from "@material-ui/core";
 import useStyles from './styles';
 import Product from "./Product/Product";
 import { useSelector, useDispatch } from "react-redux";
@@ -18,14 +18,44 @@ const Products = ()=>{
     },[dispatch])
 
     const products = useSelector((state) => state.products.products);
+
+    const [search, setSearch ] = useState('')
+    const [seachItems, setSearchItems ] = useState(null);
+
     const single = (id)=>{
         history.push(`/product/${id}`)
     }
 
+    useEffect(()=>{
+        let se = products?.filter(item=> item?.ItemName?.toLowerCase().includes(search.toLowerCase()))
+        console.log(se)
+        setSearchItems(se);
+    }, [products, search])
+
+    // return(
+    //     !products?.length ? <CircularProgress/> : (
+    //         <Grid className={classes.mainContainer} container alignItems={"stretch"} spacing={3}>
+    //             <TextField name={"ItemName"} variant={'outlined'} label={"Search Products"} fullWidth value={search}
+    //                        onChange={ (e) => setSearch(e.target.value)}
+    //             />
+    //             {products.map((product) =>(
+    //                 <Grid key={product._id} item xs={12} sm={4}>
+    //                     <div onClick={()=>single(product._id)}>
+    //                         <Product product={product} ></Product>
+    //                     </div>
+
+    //                 </Grid>
+    //             ))}
+    //         </Grid>
+    //     )
+    //  );
     return(
-        !products?.length ? <CircularProgress/> : (
+        !seachItems?.length ? <CircularProgress/> : (
             <Grid className={classes.mainContainer} container alignItems={"stretch"} spacing={3}>
-                {products.map((product) =>(
+                <TextField name={"ItemName"} variant={'outlined'} label={"Search Products"} fullWidth value={search}
+                           onChange={ (e) => setSearch(e.target.value)}
+                />
+                {seachItems.map((product) =>(
                     <Grid key={product._id} item xs={12} sm={4}>
                         <div onClick={()=>single(product._id)}>
                             <Product product={product} ></Product>
