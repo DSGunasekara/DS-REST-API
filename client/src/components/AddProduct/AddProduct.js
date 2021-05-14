03e821bdfdfb28e4a676c8d560c5d873312d8058
Dilain Gunasekara <adsgunasekara18@gmail.com>
2021-05-14 15:03:13 +0530

    create product alert added

--- client/src/components/AddProduct/AddProduct.js
+++ client/src/components/AddProduct/AddProduct.js
@@ -1,9 +1,10 @@
-import React, {useState, hi} from "react";
+import React, {useState} from "react";
 import useStyles from './styles';
-import {TextField, Button, Typography, Paper} from "@material-ui/core";
+import {TextField, Button, Typography, Paper, Snackbar} from "@material-ui/core";
 import {useDispatch } from "react-redux";
 import {createProduct} from "../../actions/products";
 import FileBase from "react-file-base64";
+import { Alert, AlertTitle } from '@material-ui/lab';
 // import { useHistory } from 'react-router-dom';
 
 const AddProduct = () =>{
@@ -18,15 +19,17 @@ const AddProduct = () =>{
         seller: user
     });
 
+    const [open, setOpen] = React.useState(false);
+
     const classes = useStyles();
     const dispatch = useDispatch();
     // const history = useHistory();
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
-            dispatch(createProduct(productData));
-            // history.push('/profile')
-        //    clear();
+        dispatch(createProduct(productData));
+        setOpen(true)
+        setProductData({ItemName: '', price: '', description: '', images: '', quantity: ''})
     };
 
     if(!user){
@@ -39,40 +42,56 @@ const AddProduct = () =>{
         )
     }
 
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen(false);
+      };
+
      const clear = () =>{
     //     setCurrentId(null);
     //     setProductData({ItemName:'', price:'', description:'', images:'', quantity:''});
      }
 
     return(
-        <Paper className={classes.paper}>
-            <form autoComplete={"off"} noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant={"h3"}>Add a New Product</Typography>
-                <TextField name={"ItemName"} variant={'outlined'} label={"ItemName"} fullWidth value={productData.ItemName}
-                           onChange={ (e) => setProductData({...productData, ItemName: e.target.value})}
-                />
+        <>
+        {
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success">
+                    <AlertTitle>Item Added to Cart</AlertTitle>
+                </Alert>
+            </Snackbar>
+            }
+            <Paper className={classes.paper}>
+                <form autoComplete={"off"} noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                    <Typography variant={"h3"}>Add a New Product</Typography>
+                    <TextField name={"ItemName"} variant={'outlined'} label={"ItemName"} fullWidth value={productData.ItemName}
+                            onChange={ (e) => setProductData({...productData, ItemName: e.target.value})}
+                    />
 
-                <TextField name={"price"} variant={'outlined'} label={"price"} fullWidth value={productData.price}
-                            onChange={ (e) => setProductData({...productData, price: e.target.value})}
-                />
+                    <TextField name={"price"} variant={'outlined'} label={"price"} fullWidth value={productData.price}
+                                onChange={ (e) => setProductData({...productData, price: e.target.value})}
+                    />
 
-                <TextField name={"description"} variant={'outlined'} label={"description"} fullWidth value={productData.description}
-                    onChange={ (e) => setProductData({...productData, description: e.target.value})}
-                />
+                    <TextField name={"description"} variant={'outlined'} label={"description"} fullWidth value={productData.description}
+                        onChange={ (e) => setProductData({...productData, description: e.target.value})}
+                    />
 
-                <TextField name={"quantity"} variant={'outlined'} label={"quantity"} fullWidth value={productData.quantity}
-                    onChange={ (e) => setProductData({...productData, quantity: e.target.value})}
-                />
+                    <TextField name={"quantity"} variant={'outlined'} label={"quantity"} fullWidth value={productData.quantity}
+                        onChange={ (e) => setProductData({...productData, quantity: e.target.value})}
+                    />
 
-                <div className={classes.fileInput}>
-                    <FileBase type={"file"} mulitple={false} onDone={({base64}) => setProductData({ ...productData, images: base64})}/>
-                </div>
+                    <div className={classes.fileInput}>
+                        <FileBase type={"file"} mulitple={false} onDone={({base64}) => setProductData({ ...productData, images: base64})}/>
+                    </div>
 
-                <Button className={classes.buttonSubmit} color={"primary"} variant={"contained"} size={"large"} type={"submit"} fullWidth>create</Button>
-                <Button  color={"secondary"} variant={"contained"} size={"small"} onClick={clear} fullWidth>clear</Button>
-            </form>
-
-        </Paper>
+                    <Button className={classes.buttonSubmit} color={"primary"} variant={"contained"} size={"large"} type={"submit"} fullWidth>create</Button>
+                    <Button  color={"secondary"} variant={"contained"} size={"small"} onClick={clear} fullWidth>clear</Button>
+                </form>
+            </Paper>
+        </>
     )
 }
 
